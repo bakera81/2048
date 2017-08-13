965be32257ada394e1b034c71dc42e4a46106345
Anthony Baker <bakera81@icloud.com>
2017-08-12 23:26:39 -0400

    refactor: Board class

---
+++ board.py
@@ -0,0 +1,108 @@
+import random
+import numpy as np
+
+class Board(object):
+
+    def __init__(self):
+        board = np.array([[0,0,0,0],
+                          [0,0,0,0],
+                          [0,0,0,0],
+                          [0,0,0,0]])
+        self.board = board
+
+
+    def zeros(self):
+        output = []
+        for y, row in enumerate(self.board):
+            for x, col in enumerate(row):
+                if col == 0:
+                    output.append((x, y))
+        if len(output) == 0:
+            print('GAME OVER')
+        return output
+
+
+    def next(self):
+        val = random.choice([2,4])
+        loc = random.choice(self.zeros())
+        self.board[loc[1], loc[0]] = val
+
+# TODO: handle mutlidigit numbers better
+# TODO: use curses (for better terminal printing)
+    def show(self):
+        # +---------------+
+        # | x | x | x | x |
+        # | x | x | x | x |
+        # | x | x | x | x |
+        # | x | x | x | x |
+        # +---------------+
+        output = '+---------------+\n'
+        for row in self.board:
+            output += '| {0} | {1} | {2} | {3} |\n'.format(*row)
+        output += '+---------------+\n'
+        print(output)
+
+    # TODO: Use numpy to do all rows at once
+    def shift(self, row):
+        non_zero_row = [val for val in row if val != 0] # Drop all zeros
+        for i in range(len(non_zero_row) - 1): # Combine adjacent if possible
+            if non_zero_row[i] == non_zero_row[i + 1]:
+                non_zero_row[i] = non_zero_row[i] * 2
+                non_zero_row[i + 1] = 0
+        # To shift all elements, drop zeros and repad.
+        non_zero_row = [val for val in non_zero_row if val != 0] # Drop all zeros
+        row = non_zero_row + [0] * (4 - len(non_zero_row)) # Re-pad with zeros
+        return row
+
+
+    def inverse_shift(self, row):
+        non_zero_row = [val for val in row if val != 0] # Drop all zeros
+        for i in reversed(range(1, len(non_zero_row))): # Combine adjacent if possible
+            if non_zero_row[i] == non_zero_row[i - 1]:
+                non_zero_row[i] = non_zero_row[i] * 2
+                non_zero_row[i - 1] = 0
+        # To shift all elements, drop zeros and repad.
+        non_zero_row = [val for val in non_zero_row if val != 0] # Drop all zeros
+        row = [0] * (4 - len(non_zero_row)) + non_zero_row  # Re-pad with zeros
+        return row
+
+
+    def left(self):
+        self.board[0] = self.shift(self.board[0])
+        self.board[1] = self.shift(self.board[1])
+        self.board[2] = self.shift(self.board[2])
+        self.board[3] = self.shift(self.board[3])
+
+
+    def up(self):
+        self.board[:,0] = self.shift(self.board[:,0])
+        self.board[:,1] = self.shift(self.board[:,1])
+        self.board[:,2] = self.shift(self.board[:,2])
+        self.board[:,3] = self.shift(self.board[:,3])
+
+
+    def right(self):
+        self.board[0] = self.inverse_shift(self.board[0])
+        self.board[1] = self.inverse_shift(self.board[1])
+        self.board[2] = self.inverse_shift(self.board[2])
+        self.board[3] = self.inverse_shift(self.board[3])
+
+
+    def down(self):
+        self.board[:,0] = self.inverse_shift(self.board[:,0])
+        self.board[:,1] = self.inverse_shift(self.board[:,1])
+        self.board[:,2] = self.inverse_shift(self.board[:,2])
+        self.board[:,3] = self.inverse_shift(self.board[:,3])
+
+# TODO: create property for board
+    # @property
+    # def board(self):
+    #     return self.board
+    
+    # def getboard(self):
+    #     return self.board
+    # def setboard(self, value):
+    #     self.board = value
+    # def delboard(self):
+    #     del self.board
+    # board = property(getboard, setboard, delboard, "I'm the 'board' property.")
--- cli.py
+++ cli.py
@@ -1,117 +1,24 @@
-import random
 import numpy as np
-board = np.array([[0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0],
-                  [0,0,0,0]])
-
-def print_board(board):
-    # +---------------+
-    # | x | x | x | x |
-    # | x | x | x | x |
-    # | x | x | x | x |
-    # | x | x | x | x |
-    # +---------------+
-    output = '+---------------+\n'
-    for row in board:
-        output += '| {0} | {1} | {2} | {3} |\n'.format(*row)
-    output += '+---------------+\n'
-    print(output)
-
-
-def zeros(board):
-    output = []
-    for y, row in enumerate(board):
-        for x, col in enumerate(row):
-            if col == 0:
-                output.append((x, y))
-    if len(output) == 0:
-        print('GAME OVER')
-    return output
-
-
-def next(board):
-    val = random.choice([2,4])
-    loc = random.choice(zeros(board))
-    board[loc[1], loc[0]] = val
-    return board
-
-
-def shift(row):
-    non_zero_row = [val for val in row if val != 0] # Drop all zeros
-    for i in range(len(non_zero_row) - 1): # Combine adjacent if possible
-        if non_zero_row[i] == non_zero_row[i + 1]:
-            non_zero_row[i] = non_zero_row[i] * 2
-            non_zero_row[i + 1] = 0
-    # To shift all elements, drop zeros and repad.
-    non_zero_row = [val for val in non_zero_row if val != 0] # Drop all zeros
-    row = non_zero_row + [0] * (4 - len(non_zero_row)) # Re-pad with zeros
-    return row
-
-
-def inverse_shift(row):
-    non_zero_row = [val for val in row if val != 0] # Drop all zeros
-    for i in reversed(range(1, len(non_zero_row))): # Combine adjacent if possible
-        if non_zero_row[i] == non_zero_row[i - 1]:
-            non_zero_row[i] = non_zero_row[i] * 2
-            non_zero_row[i - 1] = 0
-    # To shift all elements, drop zeros and repad.
-    non_zero_row = [val for val in non_zero_row if val != 0] # Drop all zeros
-    row = [0] * (4 - len(non_zero_row)) + non_zero_row  # Re-pad with zeros
-    return row
-
-def left(board):
-    board[0] = shift(board[0])
-    board[1] = shift(board[1])
-    board[2] = shift(board[2])
-    board[3] = shift(board[3])
-    return board
-
-
-def up(board):
-    board[:,0] = shift(board[:,0])
-    board[:,1] = shift(board[:,1])
-    board[:,2] = shift(board[:,2])
-    board[:,3] = shift(board[:,3])
-    return board
-
-
-def right(board):
-    board[0] = inverse_shift(board[0])
-    board[1] = inverse_shift(board[1])
-    board[2] = inverse_shift(board[2])
-    board[3] = inverse_shift(board[3])
-    return board
-
-
-def down(board):
-    board[:,0] = inverse_shift(board[:,0])
-    board[:,1] = inverse_shift(board[:,1])
-    board[:,2] = inverse_shift(board[:,2])
-    board[:,3] = inverse_shift(board[:,3])
-    return board
-
-
-def run():
-    board = np.array([[0,0,0,0],
-                      [0,0,0,0],
-                      [0,0,0,0],
-                      [0,0,0,0]])
-    board = next(board)
-    board = next(board)
-    while 1:
-        print_board(board)
-        resp = input("Next Command: ")
-        if resp == 'q' or resp == 'Q':
-            break
-        elif resp == 's':
-            board = down(board)
-        elif resp == 'd':
-            board = right(board)
-        elif resp == 'w':
-            board = up(board)
-        elif resp == 'a':
-            board = left(board)
-        else:
-            print("Unrecognized command '{0}'. 'q' to quit.".format(resp))
-        board = next(board)
+from copy import copy
+from board import Board
+b = Board()
+b.next()
+b.next()
+while True:
+    b.show()
+    previous_board = copy(getattr(b, 'board'))
+    resp = input("Next Command: ")
+    if resp == 'q' or resp == 'Q':
+        break
+    elif resp == 's':
+        b.down()
+    elif resp == 'd':
+        b.right()
+    elif resp == 'w':
+        b.up()
+    elif resp == 'a':
+        b.left()
+    else:
+        print("Unrecognized command '{0}'. 'q' to quit.".format(resp))
+    if not np.array_equal(getattr(b, 'board'), previous_board): # Don't add new numbers if no move was made
+        b.next()
