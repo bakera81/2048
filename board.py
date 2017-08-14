e480f004cf09d6f195c0c2ab6fafbf652af90b2b
Anthony Baker <bakera81@icloud.com>
2017-08-13 22:42:41 -0400

    WIP: game.solve()

--- board.py
+++ board.py
@@ -4,7 +4,6 @@ import copy
 
 class Board(object):
 
-    # TODO: Implement deep copy (new object)
     def __init__(self, board=None):
         if board is None:
             self.board = np.array([[0,0,0,0],
@@ -16,6 +15,9 @@ class Board(object):
 
 
     def zeros(self):
+        """
+            Returns an array of tuples of the x,y coordinates of zeros on the board.
+        """
         output = []
         for y, row in enumerate(self.board):
             for x, col in enumerate(row):
@@ -23,10 +25,14 @@ class Board(object):
                     output.append((x, y))
         if len(output) == 0:
             print('GAME OVER')
+            raise
         return output
 
 
     def next(self):
+        """
+            Adds a 2 or a 4 to open spaces on the board at random.
+        """
         val = random.choice([2,4])
         loc = random.choice(self.zeros())
         self.board[loc[1], loc[0]] = val
@@ -34,6 +40,9 @@ class Board(object):
 # TODO: handle mutlidigit numbers better
 # TODO: use curses (for better terminal printing)
     def show(self):
+        """
+            Prints the board to the console.
+        """
         # +---------------+
         # | x | x | x | x |
         # | x | x | x | x |
@@ -48,6 +57,11 @@ class Board(object):
 
     # TODO: Use numpy to do all rows at once
     def shift(self, row):
+        """
+            Returns an array representing a row or column that has been shifted.
+            It will join all numbers that are equal (and eligible to be joined) and will move all numbers to the beginning of the array.
+            To shift in the opposite direction, see inverse_shift().
+        """
         non_zero_row = [val for val in row if val != 0] # Drop all zeros
         for i in range(len(non_zero_row) - 1): # Combine adjacent if possible
             if non_zero_row[i] == non_zero_row[i + 1]:
@@ -60,6 +74,11 @@ class Board(object):
 
 
     def inverse_shift(self, row):
+        """
+            Returns an array representing a row or column that has been shifted.
+            It will join all numbers that are equal (and eligible to be joined) and will move all numbers to the end of the array.
+            To shift in the opposite direction, see shift().
+        """
         non_zero_row = [val for val in row if val != 0] # Drop all zeros
         for i in reversed(range(1, len(non_zero_row))): # Combine adjacent if possible
             if non_zero_row[i] == non_zero_row[i - 1]:
@@ -72,6 +91,9 @@ class Board(object):
 
 
     def left(self):
+        """
+            Shift all rows to the left.
+        """
         self.board[0] = self.shift(self.board[0])
         self.board[1] = self.shift(self.board[1])
         self.board[2] = self.shift(self.board[2])
@@ -79,6 +101,9 @@ class Board(object):
 
 
     def up(self):
+        """
+            Shift all columns up.
+        """
         self.board[:,0] = self.shift(self.board[:,0])
         self.board[:,1] = self.shift(self.board[:,1])
         self.board[:,2] = self.shift(self.board[:,2])
@@ -86,6 +111,9 @@ class Board(object):
 
 
     def right(self):
+        """
+            Shift all rows to the right.
+        """
         self.board[0] = self.inverse_shift(self.board[0])
         self.board[1] = self.inverse_shift(self.board[1])
         self.board[2] = self.inverse_shift(self.board[2])
@@ -93,6 +121,9 @@ class Board(object):
 
 
     def down(self):
+        """
+            Shift all columns down.
+        """
         self.board[:,0] = self.inverse_shift(self.board[:,0])
         self.board[:,1] = self.inverse_shift(self.board[:,1])
         self.board[:,2] = self.inverse_shift(self.board[:,2])
@@ -100,7 +131,42 @@ class Board(object):
 
 
     def equals(self, other):
+        """
+            Returns True if the boards are equal, False otherwise.
+        """
         return np.array_equal(self.board, other.board)
+
+    # TODO: Just look at the row/col and compute. No need to create a new board.
+    def can_combine(self, direction, x, y):
+        """
+            @param direction the direction to test combining. Possible values are 'up', 'down', 'left', 'right'.
+            @param x the x coordinate of the location to test. The origin is top left.
+            @param y the y coordinate of the location to test. The origin is top left.
+            Returns True if the value at x, y would be greater if the board was shifted in direction.
+        """
+        original_val = self.board[y, x]
+        test_board = Board(self)
+
+        if direction == 'down' or direction == 'd':
+            test_board.down()
+        elif direction == 'right' or direction == 'r':
+            test_board.right()
+        elif direction == 'left' or direction == 'l':
+            test_board.left()
+        elif direction == 'up' or direction == 'u':
+            test_board.up()
+        else:
+            print("Error: invalid string for direction.")
+            raise
+
+        test_val = test_board.board[y, x]
+
+        if test_val > original_val:
+            return True
+        else:
+            return False
+
+
 
 # TODO: create property for board
     # @property
--- game.py
+++ game.py
@@ -3,6 +3,9 @@ from board import Board
 class Game(object):
 
     def __init__(self):
+        """
+            Initializes the board with two numbers.
+        """
         self.board = Board()
         self.board.next()
         self.board.next()
@@ -30,11 +33,99 @@ class Game(object):
                 self.sequence.append({'move': 'left', 'board': Board(self.board)})
                 self.board.left()
             elif resp == 'seq':
-                print(self.sequence)
+                self.show_sequence()
             else:
                 print("Unrecognized command '{0}'. 'q' to quit.".format(resp))
             if not self.board.equals(self.previous_board): # Don't add new numbers if no move was made
                 self.board.next()
+
+
+    def increment_position(self, x, y):
+        # +---------------+
+        # | > | > | > | * |
+        # | ^ | < | < | < |
+        # | > | > | > | ^ |
+        # | ^ | < | < | < |
+        # +---------------+
+        if y == 3 or y == 1:
+            if x == 0:
+                return tuple([x, y-1])
+            else:
+                return tuple([x-1, y])
+        elif y == 2:
+            if x == 3:
+                return tuple([x, y-1])
+            else:
+                return tuple([x+1, y])
+        elif y == 0:
+            if x == 3:
+                return tuple([None, None])
+            else:
+                return tuple([x+1, y])
+        else:
+            print("Error: y is out of bounds.")
+            raise
+
+# Note: change direction depending on row!
+    def solve_at_position(self, x, y):
+        if self.board.can_combine('d', x, y):
+            self.board.down()
+            return self.solve_at_position(x, y)
+        elif self.board.can_combine('r', x, y):
+            self.board.right()
+            return self.solve_at_position(x, y)
+        elif self.board.can_combine('l', x, y):
+            self.board.right()
+            return self.solve_at_position(x, y)
+        elif self.board.can_combine('u', x, y):
+            self.board.right()
+            return self.solve_at_position(x, y)
+        else:
+            return self.solve_at_position(self.increment_position(x, y))
+
+
+    def solve_at_position_2(self, x, y):
+        if self.board.can_combine('d', x, y):
+            self.board.down()
+            return True
+        elif self.board.can_combine('r', x, y):
+            self.board.right()
+            return True
+        elif self.board.can_combine('l', x, y):
+            self.board.right()
+            return True
+        elif self.board.can_combine('u', x, y):
+            self.board.right()
+            return True
+        else:
+            return False
+
+
+
+    # NOTE: The board below will be shifted to the left
+    # +---------------+
+    # | 0 | 0 | 0 | 0 |
+    # | 0 | 0 | 0 | 4 |
+    # | 0 | 0 | 0 | 4 |
+    # | 0 | 0 | 0 | 2 |
+    # +---------------+
+    def solve(self):
+        x = 3 # start solving at the bottom right
+        y = 3
+        while True:
+            print(str(x) + ', ' + str(y))
+            self.board.show()
+            action_taken = self.solve_at_position_2(x, y)
+            if action_taken == False:
+                x, y = self.increment_position(x, y)
+            else:
+                x, y = (3, 3)
+                self.board.next() #It seems like new numbers are being added at the wrong time.
+            if x == None or y == None:
+                print('Ending.')
+                break
+
+
 
     def show_sequence(self):
         for turn in self.sequence:
